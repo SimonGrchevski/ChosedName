21ddf6dd7c5e8498c2c5fbdcefc985d15ff692f8
Simon Grchevski <simongrcevski@gmail.com>
2021-11-26 14:13:46 +0100

    Refactor tests

--- spec/integration/Products/index_spec.rb
+++ spec/integration/Products/index_spec.rb
@@ -2,21 +2,16 @@ require 'rails_helper'
 
 RSpec.describe 'Group view', type: :feature do
   include Devise::Test::IntegrationHelpers
-
   let(:user) { User.create(name: 'ooyytt', email: 'waleter@email.com', password: 'Almighty') }
 
   describe '#index' do
     before do
-      Product.create(name: 'eeeeww', image: '', user_id: user.id, amount:5)
       sign_in user
       visit products_path
-      
     end
 
     context 'when rendering index page' do
       it {expect(page).to have_content('Products')}
-      # it { expect(page).to have_content('ooyytt') }
-      # it { expect(page).to have_content('5') }
     end
 
     context 'click on Add new product' do
